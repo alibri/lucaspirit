f0bf104ee087a9aeccae16529c12ed07f7c44372
guestalibri <guest@alibri.cat>
2023-05-10 09:10:11 +0000

    gitignore

--- wp-content/uploads/sucuri/sucuri-ignorescanning.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=ignorescanning;
-// created_on=1683709089;
-// updated_on=1683709089;
-exit(0);
-?>
--- wp-content/uploads/sucuri/sucuri-integrity.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=integrity;
-// created_on=1683709090;
-// updated_on=1683709090;
-exit(0);
-?>
