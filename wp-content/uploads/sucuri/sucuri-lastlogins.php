16cc2e781be6c304b8cf09089b04197a2f2470ee
guestalibri <guest@alibri.cat>
2023-05-16 08:16:06 +0000

    htaccess

---
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -0,0 +1,4 @@
+<?php exit(0); ?>
+{"user_id":1,"user_login":"Admin","user_remoteaddr":"79.153.170.252","user_hostname":"252.red-79-153-170.dynamicip.rima-tde.net","user_lastlogin":"2023-05-12 07:12:42"}
+{"user_id":3,"user_login":"developer","user_remoteaddr":"79.153.170.252","user_hostname":"252.red-79-153-170.dynamicip.rima-tde.net","user_lastlogin":"2023-05-12 07:20:47"}
+{"user_id":3,"user_login":"developer","user_remoteaddr":"181.230.210.67","user_hostname":"67-210-230-181.cab.prima.com.ar","user_lastlogin":"2023-05-12 11:44:58"}
