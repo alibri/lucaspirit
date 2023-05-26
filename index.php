5f71968bde3c790f46908dc3b764caaccaaf4c23
Alejandro López <alopez@alibri.cat>
2023-05-26 07:53:53 +0200

    maergin

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
         </div>
     </section>
 
-    <section id="ficha" class="pt-5" style="background-image: url('assets/img/bg-ficha.jpg');">
+    <section id="ficha" class="mt-5 pt-5" style="background-image: url('assets/img/bg-ficha.jpg');">
         <div class="container text-center">
             <div class="row mt-5">
                 <div class="col-lg-8 mx-auto">
